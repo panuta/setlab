cdd40cc1f1a5931bd7ecf39ce559218d14a94a39
Panu Tangchalermkul <panuta@gmail.com>
2019-04-02 22:41:30 +0700

    Finished stock price spider from settrade

--- crawlers/crawlers/items/stock_price.py
+++ crawlers/crawlers/items/stock_price.py
@@ -1,18 +1,19 @@
+# -*- coding: utf-8 -*-
 import scrapy
 
 
 class StockPrice(scrapy.Item):
     symbol = scrapy.Field()
-    date = scrapy.Field()
-    price_open = scrapy.Field()
-    price_high = scrapy.Field()
-    price_low = scrapy.Field()
-    price_avg = scrapy.Field()
-    price_close = scrapy.Field()
-    price_change = scrapy.Field()
-    price_change_percentage = scrapy.Field()
-    trade_volume = scrapy.Field()
-    trade_value = scrapy.Field()
+    date = scrapy.Field()  # วันที่
+    price_open = scrapy.Field()  # ราคาเปิด
+    price_high = scrapy.Field()  # ราคาสูงสุด
+    price_low = scrapy.Field()  # ราคาต่ำสุด
+    price_avg = scrapy.Field()  # ราคาเฉลี่ย
+    price_close = scrapy.Field()  # ราคาปิด
+    price_change = scrapy.Field()  # เปลี่ยนแปลง
+    price_change_percentage = scrapy.Field()  # %เปลี่ยนแปลง
+    trade_volume = scrapy.Field()  # ปริมาณ (พันหุ้น)
+    trade_value = scrapy.Field()  # มูลค่า (ล้านบาท)
 
     # MongoDB
     UNIQUE_KEYS = ['symbol', 'date']
--- crawlers/crawlers/spiders/settrade/stock_price.py
+++ crawlers/crawlers/spiders/settrade/stock_price.py
@@ -6,17 +6,13 @@ from decimal import Decimal
 from crawlers.items.stock_price import StockPrice
 
 
-class SETTradeSpider(scrapy.Spider):
-    name = 'settrade'
+class SETTradeStockPriceSpider(scrapy.Spider):
+    name = 'settrade_stock_price'
 
     # MongoDB
     collection_name = 'stock_price'
     unique_indexes = [('symbol', 1), ('date', 1)]
 
-    # TODO => How to query by date range
-
-    # db.getCollection('stock_price').createIndex({"symbol": 1, "date": 1}, {unique: true})
-    #
     def __init__(self, symbols=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
@@ -27,30 +23,26 @@ class SETTradeSpider(scrapy.Spider):
 
     def start_requests(self):
         for symbol in self.symbol_list:
-            request_url = 'https://www.settrade.com/C04_02_stock_historical_p1.jsp?txtSymbol={}&selectPage=2'.format(
-                symbol.upper())
+            request_url = 'https://www.settrade.com/' \
+                          'C04_02_stock_historical_p1.jsp?' \
+                          'txtSymbol={}&selectPage=2&max=200&offset=0'.format(symbol.upper())
             yield scrapy.Request(request_url, self.parse, meta={'symbol': symbol})
 
     def parse(self, response):
-        price_table = response.xpath('//div[@id="maincontent"]/descendant::table')[0]
+        tables = response.xpath('//div[@id="maincontent"]/descendant::table')
+
+        if not tables:
+            self.logger.error('Parse Error: Cannot find price table')
+            return
+
+        price_table = tables[0]
 
         for table_row in price_table.xpath('.//tbody/tr'):
             values = table_row.xpath('.//td/text()').getall()
 
-            """
-            วันที่
-            ราคาเปิด
-            ราคาสูงสุด
-            ราคาต่ำสุด
-            ราคาเฉลี่ย
-            ราคาปิด
-            เปลี่ยนแปลง
-            %เปลี่ยนแปลง
-            ปริมาณ (พันหุ้น)
-            มูลค่า (ล้านบาท)
-            SET Index
-            %เปลี่ยนแปลง
-            """
+            if len(values) != 12:
+                self.logger.warn('Parse Error: Table schema might changed')
+                continue
 
             yield StockPrice(
                 symbol=response.meta['symbol'],
@@ -65,5 +57,3 @@ class SETTradeSpider(scrapy.Spider):
                 trade_volume=Decimal(values[8].replace(',', '')),
                 trade_value=Decimal(values[9].replace(',', '')),
             )
-
-            # TODO => Do update_or_create
